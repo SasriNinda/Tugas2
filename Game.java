3db949b9bc0e4a96d1ffb2ab191677f2b2026b8b
Sasri Ninda <aanrufa@gmail.com>
2018-11-16 23:52:34 +0700

    Project Baru

---
+++ Game.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Game
+{
+    String pilihan;
+    String d;
+    String e;
+    boolean b = true;
+    boolean c = true;
+    boolean p = true;
+    int jawab;
+    int jawab1;
+    int jumlah;
+    int jumlah1;
+    int tambahSkor;
+    Scanner a = new Scanner(System.in);
+    public void Tampil()
+    {
+        System.out.println("Pilihan Game     : ");
+        System.out.println("Game Penjumlahan ");
+        System.out.println("Game Pengurangan ");
+    }
+}
